a31bb11cb99ef103127db929ff5a7399059ff9c9
”Ivan <bohdan.dz.92@gmail.com>
2020-08-25 14:43:00 -0400

    add about page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-function onClickMenu (){
-    document.getElementById("menu").classList.toggle("change")
-    document.getElementById("nav").classList.toggle("change")
-    document.getElementById("menu-bg").classList.toggle("change-bg")
-}
+
+document.querySelector('.burger').addEventListener('click', function (e){
+    e.preventDefault();
+    if (this.classList.contains('is-active')){
+        this.classList.remove('is-active');
+        document.querySelector('#menu').classList.remove('nav-active');
+        document.querySelector.classList.remove('menu_wrapper-active');
+    } else {
+        this.classList.add('is-active');
+        document.querySelector('#menu').classList.add('nav-active');
+        document.querySelector.add('menu_wrapper-active');
+    }
+})
