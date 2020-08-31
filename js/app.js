02dfb81c64338784dd76b7472effa1e2f49b9176
”Ivan <bohdan.dz.92@gmail.com>
2020-08-31 13:17:03 -0400

    fix bugs

--- js/app.js
+++ js/app.js
@@ -3,10 +3,8 @@ document.querySelector('.burger').addEventListener('click', function (e){
     if (this.classList.contains('is-active')){
         this.classList.remove('is-active');
         document.querySelector('#menu').classList.remove('nav-active');
-        document.querySelector.classList.remove('menu_wrapper-active');
     } else {
         this.classList.add('is-active');
         document.querySelector('#menu').classList.add('nav-active');
-        document.querySelector.add('menu_wrapper-active');
     }
 })
